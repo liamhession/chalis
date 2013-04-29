086766a36316982770082a455e5655e1b59570d6
zfei <feizhiye@yahoo.com>
2013-04-29 10:54:11 -0500

    Hook up listeners on buttons on join pages.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function(){
     
-    // click handler for the submit button
+    // click handler for the submit button on frontpage.html
     $('#new-challenge-short').click(function(evt){
         evt.preventDefault();
         $.ajax({
@@ -13,8 +13,8 @@ $(document).ready(function(){
         });
     });
 
-    // click handler for the submit button
-    // TODO: I am now passing 'prize' as a string array. Haven't implement tabbed text area yet. Only passing first prize desc.
+    // click handler for the submit button on details.html
+    // TODO: I am now passing 'prize' as a string array. Haven't implement tabbed text area yet.
     $('#new-challenge-detailed').click(function(evt){
         evt.preventDefault();
         $.ajax({
@@ -26,13 +26,43 @@ $(document).ready(function(){
         });
     });
 
-    // click handler for the submit button
+    // click handler for the submit button on challengers.html
     $('#add-invitee').click(function(evt){
         evt.preventDefault();
         $.ajax({
             url: '/' + challenge_name + '/send-invite',
             type: 'POST',
             data: {'email': $('#invitee-email').val()}
+        });
+    });
+
+    // click handler for the submit button on reddit-join.html
+    $('#reddit-submit').click(function(evt){
+        evt.preventDefault();
+        $.ajax({
+            url: '/' + challenge_name + '/join',
+            type: 'POST',
+            data: {'relevant_logins': $('#reddit-user').val()}
+        });
+    });
+
+    // click handler for the submit button on gen-join.html
+    $('#gen-accept').click(function(evt){
+        evt.preventDefault();
+        $.ajax({
+            url: '/' + challenge_name + '/join',
+            type: 'POST',
+            data: {'gen_join': true}
+        });
+    });
+
+    // click handler for the submit button on geo-join.html
+    $('#geo-accept').click(function(evt){
+        evt.preventDefault();
+        $.ajax({
+            url: '/' + challenge_name + '/join',
+            type: 'POST',
+            data: {'geo_join': true}
         });
     });
 
